169b5a96beab939849a19ac0c1ef12a781e13a9b
“Lester <lester@applester.co>
2014-08-22 11:53:36 +0800

    bugs check
    symfony not installing

--- manifests/symfony.pp
+++ manifests/symfony.pp
@@ -1,16 +1,15 @@
-define txtcmdr::symfony( $path = "/vagrant" ) {
+define txtcmdr::symfony(
+    $path = '/vagrant'
+) {
 
-  file { $path:
-    ensure  => directory,
-  }
+  file { $path: ensure  => directory }
 
   exec { "symfony-install":
-    command     => "composer --prefer-dist --no-interaction create-project symfony/framework-standard-edition ./",
+    command     => "composer --cache-dir=/dev/null --verbose --no-interaction create-project symfony/framework-standard-edition ./",
     cwd         => $path,
-    environment => ['COMPOSER_HOME=/usr/bin/', 'COMPOSER_PROCESS_TIMEOUT=4000'],
     creates     => "${path}/web",
     timeout     => 0,
-    require     => File[ $path ],
+    require     => File[$path],
   }
 
 }
