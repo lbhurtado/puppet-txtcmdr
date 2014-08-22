bb655e4ccb523f3a8493d4eb52a45af57bbd9598
“Lester <lester@applester.co>
2014-08-23 00:34:07 +0800

    bugs
    exec command not working

--- manifests/symfony.pp
+++ manifests/symfony.pp
@@ -4,10 +4,11 @@ define txtcmdr::symfony(
 
   file { $path: ensure  => directory }
 
-  exec { "symfony-install":
-    command     => "composer --cache-dir=/dev/null --verbose --no-interaction create-project symfony/framework-standard-edition ./",
-    cwd         => $path,
-    creates     => "${path}/web",
+  exec { 'composer --prefer-dist --no-interaction create-project symfony/framework-standard-edition ./':
+    /*command     => 'composer --verbose --no-interaction create-project symfony/framework-standard-edition',*/
+    cwd         => "$path",
+    environment => 'COMPOSER_HOME=/usr/bin/',
+    creates     => "$path/web",
     timeout     => 0,
     require     => File[$path],
   }
