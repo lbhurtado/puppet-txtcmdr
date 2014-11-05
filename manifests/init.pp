0626c00a60fd7ca01e0cad930066ac8c47d1b8e5
“Lester <lester@applester.co>
2014-11-05 13:01:28 +0800

    added manifest/init.pp

---
+++ manifests/init.pp
@@ -0,0 +1,18 @@
+
+  file{'/etc/txtcmdr':
+    ensure => directory,
+    purge => true,
+  }
+  
+  file{'/etc/txtcmdr/postfix.sql':
+    ensure  => present,
+    source  => 'puppet:///modules/txtcmdr/postfix/postfix.sql',
+    require => File['/etc/txtcmdr'],
+  }
+  
+  file{'/etc/txtcmdr/map.erb':
+    ensure  => present,
+    source  => 'puppet:///modules/txtcmdr/postfix/map.erb',
+    require => File['/etc/txtcmdr'],
+  }
+
