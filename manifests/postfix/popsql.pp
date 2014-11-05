588ba7043eaa2945acfba5fa4958fe7e37c570ef
“Lester <lester@applester.co>
2014-11-05 20:10:49 +0800

    added txtcmdr::postfix::popsql

--- manifests/postfix/popsql.pp
+++ manifests/postfix/popsql.pp
@@ -1,13 +1,13 @@
-# = Define: txtcmdr::postfix::maptemplate
+# = Define: txtcmdr::postfix::popsql
 # Based on the example42 php module, customised here to match the paths on apache 2.4.6
 #
-define txtcmdr::postfix::maptemplate ($config_dir = '/etc/txtcmdr') {
+define txtcmdr::postfix::popsql ($config_dir = '/etc/txtcmdr') {
 
   file {"${config_dir}": ensure => directory}
 
   file {"${config_dir}/${title}":
     ensure  => present,
-    source  => 'puppet:///modules/txtcmdr/postfix/map.erb',
+    source  => 'puppet:///modules/txtcmdr/postfix/postfix.sql',
     require => File["${config_dir}"],
   }
 }
