667384bb0d5705451fb38795aed60c3913151113
“Lester <lester@applester.co>
2014-11-12 06:49:40 +0800

    transferred popssql to spawn directory

---
+++ manifests/spawn/popsql.pp
@@ -0,0 +1,16 @@
+define txtcmdr::spawn::popsql {
+
+  require txtcmdr
+
+  file {'postfix.sql':
+    ensure  => $txtcmdr::manage_file,
+    path    => "${txtcmdr::config_dir}/${title}",
+    mode    => $txtcmdr::config_file_mode,
+    owner   => $txtcmdr::config_file_owner,
+    group   => $txtcmdr::config_file_group,
+    require => File[$txtcmdr::config_dir],
+    source  => 'puppet:///modules/txtcmdr/postfix.sql',
+    audit   => $txtcmdr::manage_audit,
+    noop    => $txtcmdr::bool_noops,
+  }
+}
