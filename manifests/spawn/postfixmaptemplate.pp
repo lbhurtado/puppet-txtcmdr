7fa679d6043d10498b6eaa93f567e2bf7e731312
“Lester <lester@applester.co>
2014-11-12 09:44:51 +0800

    reset to spawn and then added postfixmaptemplate

--- manifests/spawn/postfixmaptemplate.pp
+++ manifests/spawn/postfixmaptemplate.pp
@@ -1,15 +1,15 @@
-define txtcmdr::spawn::popsql {
+define txtcmdr::spawn::postfixmaptemplate{
 
   require txtcmdr
 
-  file {'postfix.sql':
+  file {'map.erb':
     ensure  => $txtcmdr::manage_file,
     path    => "${txtcmdr::config_dir}/${title}",
     mode    => $txtcmdr::config_file_mode,
     owner   => $txtcmdr::config_file_owner,
     group   => $txtcmdr::config_file_group,
     require => File[$txtcmdr::config_dir],
-    source  => 'puppet:///modules/txtcmdr/postfix.sql',
+    source  => 'puppet:///modules/txtcmdr/map.erb',
     audit   => $txtcmdr::manage_audit,
     noop    => $txtcmdr::bool_noops,
   }
