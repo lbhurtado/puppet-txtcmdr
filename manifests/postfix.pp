80675ebd20a59baafe5a53678047971a2d099946
Vagrant User <vagrant@vagrant.txtcmdr.xyz>
2014-11-24 17:42:32 -0800

    added aliases.erb

--- manifests/postfix.pp
+++ manifests/postfix.pp
@@ -89,6 +89,12 @@ class txtcmdr::postfix(
     ],
   }
 
+  file { 'aliases':
+    path    => '/etc/postfix/aliases',
+    content => template( 'txtcmdr/postfix/aliases.erb' ), 
+    require => Package[ 'postfix' ], 
+  }
+
   if defined( Class[ 'txtcmdr::dovecot' ] ) { 
     if $aliases_source {
       file{ '/etc/aliases-dovecot':
@@ -105,88 +111,6 @@ class txtcmdr::postfix(
       notify      => Service[ 'postfix' ],
     }
   }
-
-/************* ssl key/certificate *************/ 
-  if $ssl_key_content and $ssl_cert_content {
-
-    $enable_ssl = true
-
-    file { '/etc/ssl/certs/postfix-ca.crt':
-      content => $ssl_ca_content,
-      owner   => 'root',
-      group   => 'postfix',
-      mode    => 640,
-      notify  => Service[ 'postfix' ],
-    }
-
-    file { '/etc/ssl/certs/postfix.crt':
-      content => $ssl_cert_content,
-      owner   => 'root',
-      group   => 'postfix',
-      mode    => 640,
-      notify  => Service[ 'postfix' ],
-    }
-
-    file { '/etc/ssl/private/postfix.key':
-      content => $ssl_key_content,
-      owner   => 'root',
-      group   => 'postfix',
-      mode    => 640,
-      notify  => Service[ 'postfix' ],
-    }
-  } else {
-
-    $enable_ssl = false
-
-  }
-/************* ssl key/certificate *************/ 
-
-/************* /etc/postfix/transport *************/ 
-/*
-  file { '/etc/postfix/transport':
-    content => template( 'txtcmdr/postfix/transport' ), 
-    require => Package[ 'postfix' ],
-  }
-
-  exec{ '/usr/sbin/postmap hash:/etc/postfix/transport':
-    refreshonly => true,
-    subscribe   => File[ '/etc/postfix/transport' ],
-    notify 	=> Service[ 'postfix' ],
-  }
-*/
-/************* /etc/postfix/transport *************/ 
-
-/************* sender access content  *************/ 
-/*
-  if $sender_access_content {
-    file{ '/etc/postfix/sender_access':
-      content => $sender_access_content,
-      require => Package[ 'postfix' ],
-    }
-    exec{ '/usr/sbin/postmap hash:/etc/postfix/sender_access':
-      refreshonly => true,
-      subscribe   => File[ '/etc/postfix/sender_access' ],
-      notify      => Service[ 'postfix' ],
-    }
-  }
-*/
-/************* sender access content  *************/ 
-
-/************* recipient access content  *************/ 
-/*
-  if $recipient_access_content {
-    file{ '/etc/postfix/recipient_access':
-      content => $recipient_access_content,
-      require => Package[ 'postfix' ],
-    }
-    exec{ '/usr/sbin/postmap hash:/etc/postfix/recipient_access':
-      refreshonly => true,
-      subscribe   => File[ '/etc/postfix/recipient_access' ],
-      notify      => Service[ 'postfix' ],
-    }
-  }
-*/
-/************* recipient access content  *************/ 
 
   cron{ 'google-aspmx-ipv4-fix':
     command => 'for d in aspmx.l.google.com alt1.aspmx.l.google.com alt2.aspmx.l.google.com alt3.aspmx.l.google.com alt4.aspmx.l.google.com aspmx2.googlemail.com aspmx3.googlemail.com aspmx4.googlemail.com aspmx5.googlemail.com ; do sed -i "/$d/d" /etc/hosts ; echo $(dig $d A +short) $d  >> /etc/hosts; done',
